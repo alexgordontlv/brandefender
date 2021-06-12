8f3883e72635f6544e6f4443e878ba455f2acabd
alexgordontlv <alexaybn@gmail.com>
2021-06-12 23:28:33 +0300

    added currency

--- src/App.js
+++ src/App.js
@@ -1,89 +1,53 @@
 import './App.css';
 import React, { useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
 
-const data1 = [
-	{
-		name: 'Page A',
-		uv: 4000,
-		pv: 2400,
-		amt: 2400,
-	},
-	{
-		name: 'Page B',
-		uv: 3000,
-		pv: 1398,
-		amt: 2210,
-	},
-	{
-		name: 'Page C',
-		uv: 2000,
-		pv: 9800,
-		amt: 2290,
-	},
-	{
-		name: 'Page D',
-		uv: 2780,
-		pv: 3908,
-		amt: 2000,
-	},
-	{
-		name: 'Page E',
-		uv: 1890,
-		pv: 4800,
-		amt: 2181,
-	},
-	{
-		name: 'Page F',
-		uv: 2390,
-		pv: 3800,
-		amt: 2500,
-	},
-	{
-		name: 'Page G',
-		uv: 3490,
-		pv: 4300,
-		amt: 2100,
-	},
-];
-
 function App() {
 	const [date, setDate] = useState(new Date());
-	const props2 = useQuery([('dateData', date)], async () => {
-		if (date > new Date()) {
-			alert('Please pick a valid date');
-			setDate(new Date());
-			return;
-		}
+	const [currentNisAmount, setCurrentNisAmount] = useState(3.17);
+	const [prevDate, setPrevDate] = useState(null);
+	const [currency, setCurrency] = useState('USD');
+
+	const { isFetching, error, data } = useQuery([('dateData', date)], async () => {
 		const { data } = await axios.get(`http://localhost:5000/crypto-data/${date}`);
 		const dataMap = [];
-
 		for (let [key, value] of Object.entries(data)) {
 			if (key.includes('USD')) {
 				dataMap.push({
 					state: key.split(' ')[1],
-					value: value,
+					valueUSD: value,
+					valueNIS: (currentNisAmount * value).toString(),
 				});
 			}
 		}
+		setPrevDate(dataMap);
 		return dataMap;
 	});
-	const { isLoading, error, data, isFetchedAfterMount } = props2;
+
+	const handleDatePick = (dayPicked) => {
+		if (dayPicked > new Date()) {
+			alert('Please pick a valid date');
+			setDate(new Date());
+			return;
+		}
+		setDate(dayPicked);
+	};
 
 	if (error) return <div>error</div>;
-
+	console.log(data);
 	return (
 		<div className='app'>
 			<div className='container'>
-				<DayPickerInput value={date.toDateString()} placeholder={date.toDateString()} onDayChange={(day) => setDate(day)} />{' '}
+				<button onClick={() => setCurrency(currency === 'USD' ? 'NIS' : 'USD')}></button>
+				<DayPickerInput value={date.toDateString()} onDayChange={(day) => handleDatePick(day)} />{' '}
 				<LineChart
 					width={700}
 					height={300}
-					data={data}
+					data={isFetching ? prevDate : data}
 					margin={{
 						top: 5,
 						right: 30,
@@ -92,9 +56,9 @@ function App() {
 					}}>
 					<CartesianGrid strokeDasharray='3 3' />
 					<XAxis dataKey='state' />
-					<YAxis dataKey='value' />
+					<YAxis />
 					<Tooltip />
-					<Line type='monotone' dataKey='value' stroke='#82ca9d' />
+					<Line type='monotone' dataKey={currency === 'USD' ? 'valueUSD' : 'valueNIS'} stroke='#82ca9d' />
 				</LineChart>
 			</div>
 		</div>
